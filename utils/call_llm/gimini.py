eb5d3212f14986bdb861ec80519fd0b35133662e
jim <jimchung@xsg.com.tw>
2025-06-04 10:28:10 +0800

    add google gemini llm

---
+++ utils/call_llm/gimini.py
@@ -0,0 +1,25 @@
+import os
+from google import genai
+from dotenv import load_dotenv
+
+load_dotenv()
+
+# Async version of the simple wrapper, using Anthropic
+def call_llm(prompt):
+    """Async wrapper for Anthropic API call."""
+    client = genai.Client(api_key=os.environ.get("GOOGLE_API_KEY"))
+    response = client.models.generate_content(
+        model="gemini-2.0-flash", contents=prompt
+    )
+
+    return response.text
+
+if __name__ == "__main__":
+    def run_test():
+        print("## Testing async call_llm with Anthropic")
+        prompt = "Testing. Just say hi and hello world and nothing else."
+        print(f"## Prompt: {prompt}")
+        response = call_llm(prompt)
+        print(f"## Response: {response}")
+
+    run_test()
